5e2326683e6c7f0683b092d5a75e82e3ad99540e
Jon Robison <narfman0@gmail.com>
2018-09-23 14:51:47 -0400

    -Add optional host/port vars

--- newsbeuter_spread/app.py
+++ newsbeuter_spread/app.py
@@ -5,7 +5,8 @@ from flask_basicauth import BasicAuth
 
 from newsbeuter_spread.db import DB
 
-
+host = os.environ.get("HOST", "127.0.0.1")
+port = int(os.environ.get("PORT", "5000"))
 app = Flask(__name__)
 app.config["BASIC_AUTH_USERNAME"] = os.environ.get("USERNAME", None)
 app.config["BASIC_AUTH_PASSWORD"] = os.environ.get("PASSWORD", None)
@@ -30,3 +31,7 @@ def items():
 def delete(id):
     db.mark_read(id)
     return jsonify({"status": "success"})
+
+
+if __name__ == "__main__":
+    app.run(host=host, port=port)
